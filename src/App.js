a9e259d8c03038e70aa5525801960215a3e74d46
rlopezb <rlopezb@gmail.com>
2023-06-13 20:15:31 +0200

    Functional paging

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './style/App.css';
-import {Container, Table} from "react-bootstrap";
+import {Button, ButtonGroup, Container, Table} from "react-bootstrap";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
   let [pokemons, setPokemons] = useState([]);
-  let [pagination, setPagination] = useState({});
-  let getPokemons = function () {
-    axios.get("http://localhost:8080/pokemon")
+  let [pagination, setPagination] = useState({page: 0});
+  let getPokemons = () => {
+    axios.get("http://localhost:8080/pokemon?page=" + pagination.page)
         .then(response => {
           setPokemons([...response.data.content]);
           setPagination({
@@ -15,7 +15,9 @@ function App() {
             total: response.data.totalElements,
             pages: response.data.totalPages,
             first: response.data.first,
-            last: response.data.last
+            last: response.data.last,
+            start: response.data.number * response.data.size + 1,
+            end: response.data.number * response.data.size + response.data.size,
           });
         })
         .catch(error => {
@@ -23,11 +25,28 @@ function App() {
           //toast.error(error.message);
         });
   };
+  let onFirst = () => {
+    pagination.page = 0;
+    getPokemons();
+  };
+  let onPrevious = () => {
+    pagination.page--;
+    getPokemons();
+  };
+  let onNext = () => {
+    pagination.page++;
+    getPokemons();
+  };
+  let onLast = () => {
+    pagination.page = pagination.pages - 1;
+    getPokemons();
+  };
 
   useEffect(() => getPokemons(), []);
 
   return (
       <Container>
+        <h2>Pokemons</h2>
         <Table striped bordered hover>
           <thead>
           <tr>
@@ -55,10 +74,17 @@ function App() {
             </tr>
           })}
           </tbody>
-          <tfoot>
-
-          </tfoot>
         </Table>
+        <div className="float-end">
+          <ButtonGroup>
+            <Button variant="outline-secondary" onClick={onFirst} disabled={pagination.first}>&#8810;</Button>
+            <Button id="previous" variant="outline-secondary" onClick={onPrevious}
+                    disabled={pagination.first}>&#60;</Button>
+            <Button id="pagination" variant="outline-secondary">page {pagination.page+1} of {pagination.pages}</Button>
+            <Button id="next" variant="outline-secondary" onClick={onNext} disabled={pagination.last}>&#62;</Button>
+            <Button variant="outline-secondary" onClick={onLast} disabled={pagination.last}>&#8811;</Button>
+          </ButtonGroup>
+        </div>
       </Container>
   );
 }
